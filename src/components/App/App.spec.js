def5f43321ee26d8d8d29281db689093bb8b9f5e
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-11-10 21:47:41 +0100

    Fixed #5 aded test and unit testing framework

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
 describe('<App />', () => {
+  const build = passedProps => {
+    const defaultProps = { prop: 'Welcome' };
+    return shallow(<App {...defaultProps} {...passedProps} />);
+  };
+
   it('should render', () => {
-    expect(true).toBe(true);
+    const component = build();
+    expect(component).toMatchSnapshot();
   });
 });
