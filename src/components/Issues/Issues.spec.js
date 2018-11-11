c8cb9bbe60028e9c76060e19e0dd5630beeb47e1
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-11-11 20:19:28 +0100

    add connected test

--- src/components/Issues/Issues.spec.js
+++ src/components/Issues/Issues.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { IssuesNotConnected } from './Issues';
+import configureStore from 'redux-mock-store';
+import Issues, { IssuesNotConnected } from './Issues';
 
 describe('<IssuesNotConnected />', () => {
   const build = passedProps => {
@@ -11,6 +12,16 @@ describe('<IssuesNotConnected />', () => {
   it('should render', () => {
     const dispatch = jest.fn();
     const component = build({ dispatch });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should connect', () => {
+    const mockStore = configureStore();
+    const store = mockStore({
+      data: {},
+      loading: false,
+    });
+    const component = shallow(<Issues store={store} />);
     expect(component).toMatchSnapshot();
   });
 });
