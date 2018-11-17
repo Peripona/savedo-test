f9c27820ac7b491a7964d54fd3343735bf258162
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-11-17 02:47:07 +0100

    Fixes #9 added issues and issues page, with design and tests

--- src/components/Issues/Issues.js
+++ src/components/Issues/Issues.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import styles from './Issues.css';
 import { connect } from 'react-redux';
 import { searchRequest } from '../../state/issues/actions';
+import './Issues.css';
+import Octicon, { Repo } from '@githubprimer/octicons-react';
+
+import Issue from '../Issue/Issue';
 
 const issuesUrl = 'https://api.github.com/repos/facebook/react/issues';
 
@@ -9,12 +12,35 @@ class Issues extends Component {
   componentDidMount() {
     this.props.dispatch(searchRequest({ url: issuesUrl }));
   }
+
+  getIssuesList(data) {
+    return data.map((issue, index) => <Issue key={index} issue={issue} />);
+  }
+
+  getPageTitle() {
+    return (
+      <div className="mt-20">
+        <span>
+          <Octicon icon={Repo} size="small" verticalAlign="middle" />
+        </span>
+        <span className=" ml-5 project-title color-primary">
+          facebook<b>react</b>
+        </span>
+      </div>
+    );
+  }
+
   render() {
     const { fetching, data } = this.props;
     return (
-      <div className={styles.Issues}>
+      <div>
         {fetching && 'Loading...'}
-        {data && data.map(result => <div key={result.id}>{result.number}</div>)}
+        {data && data.length > 0 && (
+          <div className="issues">
+            {this.getPageTitle()}
+            <ul className="issues-list">{this.getIssuesList(data)}</ul>
+          </div>
+        )}
       </div>
     );
   }
