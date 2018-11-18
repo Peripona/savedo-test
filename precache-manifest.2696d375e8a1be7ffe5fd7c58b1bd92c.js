b59e66c30535045b8d8fcd1d5d18d650748a66bc
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-11-18 13:20:42 +0100

    Updates

---
+++ precache-manifest.2696d375e8a1be7ffe5fd7c58b1bd92c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "38170f646dea26d89f69",
+    "url": "./static/js/main.38170f64.chunk.js"
+  },
+  {
+    "revision": "76c278588db0f299c692",
+    "url": "./static/js/1.76c27858.chunk.js"
+  },
+  {
+    "revision": "38170f646dea26d89f69",
+    "url": "./static/css/main.28e9b55b.chunk.css"
+  },
+  {
+    "revision": "cafaaebc22b4aaec2fd0b6eb0cd1e915",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/savedo-test/precache-manifest.e0fda3fd13944e4b3e2c8db874204eaf.js"
+  "./precache-manifest.2696d375e8a1be7ffe5fd7c58b1bd92c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/savedo-test/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
