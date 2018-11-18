2a8d881da11576a201bf74432305ca8e355b4fb8
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-11-18 13:22:05 +0100

    Updates

---
+++ precache-manifest.ae8b46e98ab7a0e7d3a15b8e08bfb54c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81374bc92edf2334464c",
+    "url": "/savedo-test/static/js/runtime~main.81374bc9.js"
+  },
+  {
+    "revision": "85e7127df16648f5bfab",
+    "url": "/savedo-test/static/js/main.85e7127d.chunk.js"
+  },
+  {
+    "revision": "76c278588db0f299c692",
+    "url": "/savedo-test/static/js/1.76c27858.chunk.js"
+  },
+  {
+    "revision": "85e7127df16648f5bfab",
+    "url": "/savedo-test/static/css/main.28e9b55b.chunk.css"
+  },
+  {
+    "revision": "5f9948688d6cd5b065391d7c8ced88cc",
+    "url": "/savedo-test/index.html"
+  }
+];
